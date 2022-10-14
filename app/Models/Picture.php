e9c9f0098d7aff526b6b4a5520c3568ac5b24827
esperantkela <esperant.kela@gmail.com>
2022-10-14 16:19:28 +0100

    picture model

--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Picture extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'image'
+    ];
 }
